15fb73103bfb7b936970221703af4b08dee160cc
abubakar-shaikh-dev <shaikhabubakar2380@gmail.com>
2023-10-13 12:19:59 +0530

    qif_quantitative_tree_data.js Created with Improper NodeIds

---
+++ src/pages/qif-quantitative/qif_quantitative_tree_data.js
@@ -0,0 +1,315 @@
+const qif_quantitative_tree_data = [
+  {
+    nodeId: "1",
+    label: "1",
+    children: [
+      {
+        nodeId: "2",
+        label: "1.1",
+        children: [
+          { nodeId: "3", label: "1.1.1" },
+          { nodeId: "4", label: "1.1.3" },
+        ],
+      },
+      {
+        nodeId: "2",
+        label: "1.2",
+        children: [
+          { nodeId: "3", label: "1.2.1" },
+          { nodeId: "4", label: "1.2.2" },
+        ],
+      },
+      {
+        nodeId: "2",
+        label: "1.3",
+        children: [
+          { nodeId: "3", label: "1.3.2" },
+          { nodeId: "4", label: "1.3.3" },
+          { nodeId: "4", label: "1.3.4" },
+        ],
+      },
+      {
+        nodeId: "2",
+        label: "1.4",
+        children: [
+          { nodeId: "3", label: "1.4.1" },
+          { nodeId: "4", label: "1.4.2" },
+        ],
+      },
+    ],
+  },
+  {
+    nodeId: "7",
+    label: "2",
+    children: [
+      {
+        nodeId: "10",
+        label: "2.1",
+        children: [
+          { nodeId: "11", label: "2.1.1" },
+          { nodeId: "12", label: "2.1.2" },
+        ],
+      },
+      {
+        nodeId: "13",
+        label: "2.2",
+        children: [
+          { nodeId: "14", label: "2.2.2" },
+        ],
+      },
+      {
+        nodeId: "16",
+        label: "2.3",
+        children: [
+          { nodeId: "17", label: "2.3.3" }
+        ],
+      },
+      {
+        nodeId: "16",
+        label: "2.4",
+        children: [
+          { nodeId: "17", label: "2.4.1" },
+          { nodeId: "18", label: "2.4.2" },
+          { nodeId: "18", label: "2.4.3" },
+          { nodeId: "18", label: "2.4.4" },
+        ],
+      },
+      {
+        nodeId: "16",
+        label: "2.5",
+        children: [
+          { nodeId: "17", label: "2.5.1" },
+          { nodeId: "18", label: "2.5.2" },
+          { nodeId: "18", label: "2.5.4" },
+        ],
+      },
+      {
+        nodeId: "16",
+        label: "2.6",
+        children: [
+          { nodeId: "17", label: "2.6.3" },
+        ],
+      },
+      {
+        nodeId: "16",
+        label: "2.7",
+        children: [
+          { nodeId: "17", label: "2.7.1" },
+        ],
+      },
+    ],
+  },
+  {
+    nodeId: "19",
+    label: "3",
+    children: [
+      {
+        nodeId: "20",
+        label: "3.1",
+        children: [
+          { nodeId: "21", label: "3.1.2" },
+          { nodeId: "21", label: "3.1.3" },
+          { nodeId: "21", label: "3.1.4" },
+          { nodeId: "21", label: "3.1.5" },
+          { nodeId: "21", label: "3.1.6" },
+        ],
+      },
+      {
+        nodeId: "20",
+        label: "3.2",
+        children: [
+          { nodeId: "21", label: "3.2.1" },
+          { nodeId: "21", label: "3.2.2" },
+          { nodeId: "21", label: "3.2.3" },
+        ],
+      },
+      {
+        nodeId: "20",
+        label: "3.3",
+        children: [
+          { nodeId: "21", label: "3.3.2" },
+          { nodeId: "21", label: "3.3.3" },
+        ],
+      },
+      {
+        nodeId: "20",
+        label: "3.4",
+        children: [
+          { nodeId: "21", label: "3.4.1" },
+          { nodeId: "21", label: "3.4.2" },
+          { nodeId: "21", label: "3.4.3" },
+          { nodeId: "21", label: "3.4.4" },
+          { nodeId: "21", label: "3.4.5" },
+          { nodeId: "21", label: "3.4.6" },
+          { nodeId: "21", label: "3.4.7" },
+          { nodeId: "21", label: "3.4.8" },
+          { nodeId: "21", label: "3.4.9" }
+        ],
+      },
+      {
+        nodeId: "20",
+        label: "3.5",
+        children: [
+          { nodeId: "21", label: "3.5.2" }
+        ],
+      },
+      {
+        nodeId: "20",
+        label: "3.6",
+        children: [
+          { nodeId: "21", label: "3.6.2" },
+          { nodeId: "21", label: "3.6.3" },
+          { nodeId: "21", label: "3.6.4" },
+        ],
+      },
+      {
+        nodeId: "20",
+        label: "3.7",
+        children: [
+          { nodeId: "21", label: "3.7.1" },
+          { nodeId: "21", label: "3.7.2" },
+        ],
+      },
+    ],
+  },
+  {
+    nodeId: "24",
+    label: "4",
+    children: [
+      {
+        nodeId: "25",
+        label: "4.1",
+        children: [
+          { nodeId: "26", label: "4.1.4" },
+        ],
+      },
+      {
+        nodeId: "28",
+        label: "4.2",
+        children: [
+          { nodeId: "29", label: "4.2.2" },
+          { nodeId: "29", label: "4.2.3" },
+          { nodeId: "29", label: "4.2.4" },
+        ],
+      },
+      {
+        nodeId: "30",
+        label: "4.3",
+        children: [
+          { nodeId: "31", label: "4.3.1" },
+          { nodeId: "31", label: "4.3.3" },
+          { nodeId: "31", label: "4.3.4" },
+          { nodeId: "31", label: "4.3.5" },
+      ],
+      },
+      {
+        nodeId: "32",
+        label: "4.4",
+        children: [
+          { nodeId: "33", label: "4.4.1" },
+        ],
+      },
+    ],
+  },
+  {
+    nodeId: "34",
+    label: "5",
+    children: [
+      {
+        nodeId: "35",
+        label: "5.1",
+        children: [
+          { nodeId: "36", label: "5.1.1" },
+          { nodeId: "36", label: "5.1.2" },
+          { nodeId: "36", label: "5.1.3" },
+          { nodeId: "36", label: "5.1.4" },
+        ],
+      },
+      {
+        nodeId: "37",
+        label: "5.2",
+        children: [
+          { nodeId: "38", label: "5.2.1" },
+          { nodeId: "38", label: "5.2.2" },
+          { nodeId: "38", label: "5.2.3" },
+        ],
+      },
+      {
+        nodeId: "37",
+        label: "5.3",
+        children: [
+          { nodeId: "38", label: "5.3.1" },
+          { nodeId: "38", label: "5.3.3" }
+        ],
+      },
+      {
+        nodeId: "37",
+        label: "5.4",
+        children: [
+          { nodeId: "38", label: "5.4.2" }
+        ],
+      },
+    ],
+  },
+  {
+    nodeId: "39",
+    label: "6",
+    children: [
+      {
+        nodeId: "43",
+        label: "6.2",
+        children: [
+          { nodeId: "44", label: "6.2.3" },
+        ],
+      },
+      {
+        nodeId: "46",
+        label: "6.3",
+        children: [
+          { nodeId: "47", label: "6.3.2" },
+          { nodeId: "48", label: "6.3.3" },
+          { nodeId: "48", label: "6.3.4" },
+        ],
+      },
+      {
+        nodeId: "49",
+        label: "6.4",
+        children: [
+          { nodeId: "50", label: "6.4.2" },
+          { nodeId: "51", label: "6.4.3" },
+        ],
+      },
+      {
+        nodeId: "52",
+        label: "6.5",
+        children: [
+          { nodeId: "54", label: "6.5.2" },
+        ],
+      },
+    ],
+  },
+  {
+    nodeId: "55",
+    label: "7",
+    children: [
+      {
+        nodeId: "56",
+        label: "7.1",
+        children: [
+          { nodeId: "57", label: "7.1.2" },
+          { nodeId: "58", label: "7.1.4" },
+          { nodeId: "59", label: "7.1.5" },
+          { nodeId: "60", label: "7.1.6" },
+          { nodeId: "61", label: "7.1.7" },
+          { nodeId: "61", label: "7.1.10" },
+        ],
+      },
+    ],
+  },
+  { 
+    nodeId: "57",
+     label: "Evaluative Report"
+  }
+];
+
+export default qif_quantitative_tree_data;
