0f36a7a4c657cf518b735d6bb6839582879c85e2
abubakar-shaikh-dev <shaikhabubakar2380@gmail.com>
2023-10-13 11:23:38 +0530

    Minor Changes

--- src/pages/qif-qualitative/QifQualitative.jsx
+++ src/pages/qif-qualitative/QifQualitative.jsx
@@ -11,16 +11,16 @@ export default function QifQualitative() {
   return (
     <>
       <Navigation />
-        <TabNavigation />
-        <div className="shadow-xl p-5 my-10 mx-5 bg-white ">
-          <div className="flex flex-col gap-4">
-            <div className="flex justify-between flex-col mb-3 items-start md:flex-row md:items-center">
-              <h1 className="font-bold text-xl">
-                QIF Qualitative Metric for Trial - Dgm Ayurvedic Medical College
-              </h1>
-              <StatusBadges />
-            </div>
-            {/* <div className="flex justify-end gap-4 text-sm ">
+      <TabNavigation />
+      <div className="shadow-xl p-5 my-10 mx-5 bg-white ">
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-between flex-col mb-3 items-start md:flex-row md:items-center">
+            <h1 className="font-bold text-xl">
+              QIF Qualitative Metric for Trial - Dgm Ayurvedic Medical College
+            </h1>
+            <StatusBadges />
+          </div>
+          {/* <div className="flex justify-end gap-4 text-sm ">
               <div className="flex justify-center gap-3 items-center">
                 <span className="font-semibold">Status of the Application</span>
                 <div className="bg-green-500 px-4 py-1 text-white rounded-full mr-2 shadow-md">
@@ -29,12 +29,11 @@ export default function QifQualitative() {
               </div>
             </div> */}
 
-            <section>
-              <FolderTreeSidebar data={qif_qualitative_tree_data} />
-              {/* <Metric_1_1_1 /> */}
-            </section>
-          </div>
+          <section>
+            <FolderTreeSidebar data={qif_qualitative_tree_data} />
+          </section>
         </div>
+      </div>
     </>
   );
 }
