088d4753f8e26a96b3992513851712cf5bb02e4a
abubakar-shaikh-dev <shaikhabubakar2380@gmail.com>
2023-10-19 11:15:51 +0530

    Executive Summary is Ready to Interact with API

--- src/pages/executive-summary/ExecutiveSummary.jsx
+++ src/pages/executive-summary/ExecutiveSummary.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "../../components/partials/Navigation";
 import TabNavigation from "../../components/TabNavigation";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
@@ -8,19 +8,33 @@ import Divider from "../../components/Divider";
 import StatusBadges from "../../components/StatusBadges";
 
 export default function ExecutiveSummary() {
+  const [data, setData] = useState({
+    introductory_note: "",
+    criterion_wise_summary: "",
+    strength_weaknesses_opportunities_and_challenges: "",
+    additional_information: "",
+    over_all_conclusive_explication: "",
+  });
+
+  const onClickSubmit = () => {
+    console.log("submit data", data);
+  };
+
+  const onClickDraft = () => {
+    console.log("Draft data", data);
+  };
+
   return (
     <>
-    <Navigation />
-    <TabNavigation />
+      <Navigation />
+      <TabNavigation />
       <div className="shadow-xl p-5 my-10 mx-5 bg-white">
         <div className="flex flex-col gap-4">
           <div className="flex justify-between flex-col items-start md:flex-row md:items-center">
-              <h1 className="font-bold text-xl">
-              Executive Summary
-              </h1>
-              <StatusBadges />
-            </div>
-          
+            <h1 className="font-bold text-xl">Executive Summary</h1>
+            <StatusBadges />
+          </div>
+
           <div className="flex justify-end gap-4 text-sm">
             <div className="flex justify-center gap-3 items-center">
               <span className="font-semibold">Status of the Application</span>
@@ -29,7 +43,7 @@ export default function ExecutiveSummary() {
               </div>
             </div>
           </div>
-          
+
           <span className="font-medium">
             Every HEI applying for the A&A process shall prepare an Executive
             Summary highlighting the main features of the Institution including
@@ -45,9 +59,13 @@ export default function ExecutiveSummary() {
 
           <CKEditor
             editor={ClassicEditor}
-            onInit={(editor) => {
-              // You can store the "editor" and use when it is needed.
-              console.log("Editor is ready to use!", editor);
+            onChange={(event, editor) => {
+              setData((prevValue) => {
+                return {
+                  ...prevValue,
+                  introductory_note: editor.getData(),
+                };
+              });
             }}
           />
 
@@ -58,9 +76,13 @@ export default function ExecutiveSummary() {
           <CKEditor
             editor={ClassicEditor}
             data=""
-            onInit={(editor) => {
-              // You can store the "editor" and use when it is needed.
-              console.log("Editor is ready to use!", editor);
+            onChange={(event, editor) => {
+              setData((prevValue) => {
+                return {
+                  ...prevValue,
+                  criterion_wise_summary: editor.getData(),
+                };
+              });
             }}
           />
 
@@ -73,9 +95,14 @@ export default function ExecutiveSummary() {
           <CKEditor
             editor={ClassicEditor}
             data=""
-            onInit={(editor) => {
-              // You can store the "editor" and use when it is needed.
-              console.log("Editor is ready to use!", editor);
+            onChange={(event, editor) => {
+              setData((prevValue) => {
+                return {
+                  ...prevValue,
+                  strength_weaknesses_opportunities_and_challenges:
+                    editor.getData(),
+                };
+              });
             }}
           />
           <Divider />
@@ -84,9 +111,13 @@ export default function ExecutiveSummary() {
           <CKEditor
             editor={ClassicEditor}
             data=""
-            onInit={(editor) => {
-              // You can store the "editor" and use when it is needed.
-              console.log("Editor is ready to use!", editor);
+            onChange={(event, editor) => {
+              setData((prevValue) => {
+                return {
+                  ...prevValue,
+                  additional_information: editor.getData(),
+                };
+              });
             }}
           />
           <Divider />
@@ -95,23 +126,33 @@ export default function ExecutiveSummary() {
           <CKEditor
             editor={ClassicEditor}
             data=""
-            onInit={(editor) => {
-              // You can store the "editor" and use when it is needed.
-              console.log("Editor is ready to use!", editor);
+            onChange={(event, editor) => {
+              setData((prevValue) => {
+                return {
+                  ...prevValue,
+                  over_all_conclusive_explication: editor.getData(),
+                };
+              });
             }}
           />
 
           <div className="flex gap-3 my-5">
-            <button className="bg-indigo-500 text-sm hover:bg-indigo-700 hover:shadow-lg focus:bg-indigo-900 text-white px-5 py-2 rounded-md transition-all duration-150">
+            <button
+              className="bg-indigo-500 text-sm hover:bg-indigo-700 hover:shadow-lg focus:bg-indigo-900 text-white px-5 py-2 rounded-md transition-all duration-150"
+              onClick={onClickDraft}
+            >
               Save for Draft
             </button>
-            <button className="flex justify-center text-sm items-center bg-gray-800 hover:bg-gray-900 hover:shadow-lg focus:bg-black text-white px-5 py-2 rounded-md transition-all duration-150">
+            <button
+              className="flex justify-center text-sm items-center bg-gray-800 hover:bg-gray-900 hover:shadow-lg focus:bg-black text-white px-5 py-2 rounded-md transition-all duration-150"
+              onClick={onClickSubmit}
+            >
               <span>Submit for Review</span>
               <ArrowRightIcon className="h-4 w-4 ml-2" />
             </button>
           </div>
         </div>
       </div>
-      </>
+    </>
   );
 }
